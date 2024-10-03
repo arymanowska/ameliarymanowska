366fd38f269caf8bd541f2946cd6f0571c04b703
Amela <amelia.rym@wp.pl>
2024-10-03 08:08:34 +0200

    next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Namelist from "@/components/namelist";
+import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full">
     <Namelist imie="Jan Kowalksi"/>
-    <Namelist imie="Jan"/>
+    <Namelist imie="Jan" ok/>
     <Namelist imie="Kowalksi"/>
     <Namelist imie="Kowal"/>
     </div>
